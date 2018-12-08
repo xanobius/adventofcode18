4ccbee6c2970130fbd824ee86546b58952a87013
Philipp Lauwiner <philipp@lauwiner.name>
2018-12-08 20:40:41 +0000

    day 8, part 1

--- app/TwoEighteen/Eight.php
+++ app/TwoEighteen/Eight.php
@@ -6,8 +6,11 @@ use App\DayClass;
 
 class Eight extends DayClass
 {
-//    protected $filepath = '2018/day_08.txt';
-    protected $filepath = '2018/day_08.example.txt';
+    protected $filepath = '2018/day_08.txt';
+//    protected $filepath = '2018/day_08.example.txt';
+
+    private $inputs = null;
+    private $metaDataSum = 0;
 
     public function primary()
     {
@@ -16,14 +19,48 @@ class Eight extends DayClass
 
         set_time_limit(60);
         $inputs = $this->getInput()[0];
-        $splited = explode(' ', $inputs);
-        return $splited;
+        $this->inputs = explode(' ', $inputs);
+        for($i = 0; $i < count($this->inputs); $i++){
+            $this->inputs[$i] = (int)$this->inputs[$i];
+        }
+        $offset = $this->getMetaSum(0);
+
+        return [
+            'total' => $offset,
+            'realTotal' => count($this->inputs),
+            'metaSum' => $this->metaDataSum
+        ];
     }
 
     public function secondary()
     {
         $input = $this->getInput();
         return;
+    }
+
+    private function getMetaSum($start)
+    {
+        $children = $this->inputs[$start];
+        $metadatas = $this->inputs[$start + 1];
+
+        if($children == 0){
+            for($i = $start + 2; $i < $start + 2 + $metadatas; $i++){
+                $this->metaDataSum += $this->inputs[$i];
+            }
+            return 2 + $metadatas;
+        }else{
+            $offset = 0;
+            for($i = 0; $i < $children; $i++){
+                $offset = $offset + $this->getMetaSum($start + 2 + $offset);
+            }
+
+            for($i = $start + 2 + $offset; $i < $start + 2 + $offset + $metadatas; $i++){
+                $this->metaDataSum += $this->inputs[$i];
+            }
+            return 2 + $offset + $metadatas;
+        }
+
+
     }
 
 }
